ab6877f2b182dc1f460fd2c18b246fbc36c50888
milindkusahu <milindsahu2000@gmail.com>
2024-05-27 17:01:45 +0530

    first commit

---
+++ app.mjs
@@ -0,0 +1,62 @@
+import express from 'express';
+import gplay from 'google-play-scraper';
+import cors from 'cors';
+import path from 'path';
+import axios from 'axios';
+
+const app = express();
+app.use(cors());
+
+app.get('/screenshots', async (req, res) => {
+  const { appId } = req.query;
+
+  try {
+    const appDetails = await gplay.app({ appId });
+    const screenshots = appDetails.screenshots.map(url => `${url}=w2560-h1440-rw`);
+
+    res.json(screenshots);
+  } catch (error) {
+    console.error('Failed to fetch app screenshots:', error);
+    res.status(500).json({ error: 'Failed to fetch app screenshots' });
+  }
+});
+
+app.get('/download', async (req, res) => {
+  const { url } = req.query;
+
+  try {
+    const response = await axios({
+      url,
+      method: 'GET',
+      responseType: 'stream',
+    });
+
+    const contentType = response.headers['content-type'];
+    const fileExtension = getFileExtension(contentType);
+    const filename = `${path.basename(url)}.${fileExtension}`;
+
+    res.setHeader('Content-Disposition', `attachment; filename=${filename}`);
+    response.data.pipe(res);
+  } catch (error) {
+    console.error('Failed to download screenshot:', error);
+    res.status(500).json({ error: 'Failed to download screenshot' });
+  }
+});
+
+function getFileExtension(contentType) {
+  switch (contentType) {
+    case 'image/webp':
+      return 'webp';
+    case 'image/png':
+      return 'png';
+    case 'image/jpeg':
+      return 'jpg';
+    default:
+      return 'bin';
+  }
+}
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
