ecd329a2074948689809063c22742f7dccd27925
Milind Kumar Sahu <41789140+milindkusahu@users.noreply.github.com>
2024-05-28 09:11:36 +0530

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -1,58 +1,79 @@
-import express from 'express';
-import gplay from 'google-play-scraper';
-import cors from 'cors';
-import path from 'path';
-import axios from 'axios';
+import express from "express";
+import gplay from "google-play-scraper";
+import cors from "cors";
+import path from "path";
+import axios from "axios";
 
 const app = express();
 app.use(cors());
 
-app.get('/screenshots', async (req, res) => {
+app.get("/screenshots", async (req, res) => {
   const { appId } = req.query;
 
   try {
     const appDetails = await gplay.app({ appId });
-    const screenshots = appDetails.screenshots.map(url => `${url}=w2560-h1440-rw`);
+    const appName = appDetails.title;
+    const screenshots = appDetails.screenshots.map(
+      (url) => `${url}=w2560-h1440-rw`
+    );
 
-    res.json(screenshots);
+    res.json({ appName, screenshots });
   } catch (error) {
-    console.error('Failed to fetch app screenshots:', error);
-    res.status(500).json({ error: 'Failed to fetch app screenshots' });
+    console.error("Failed to fetch app screenshots:", error);
+    res.status(500).json({ error: "Failed to fetch app screenshots" });
   }
 });
 
-app.get('/download', async (req, res) => {
+app.get("/app-details", async (req, res) => {
+  const { appId } = req.query;
+
+  try {
+    const appDetails = await gplay.app({ appId });
+    const appName = appDetails.title;
+    const screenshots = appDetails.screenshots.map(
+      (url) => `${url}=w2560-h1440-rw`
+    );
+    const featuredGraphicsUrl = `${appDetails.headerImage}=w2560-h1440-rw`;
+
+    res.json({ appName, screenshots, featuredGraphicsUrl });
+  } catch (error) {
+    console.error("Failed to fetch app details:", error);
+    res.status(500).json({ error: "Failed to fetch app details" });
+  }
+});
+
+app.get("/download", async (req, res) => {
   const { url } = req.query;
 
   try {
     const response = await axios({
       url,
-      method: 'GET',
-      responseType: 'stream',
+      method: "GET",
+      responseType: "stream",
     });
 
-    const contentType = response.headers['content-type'];
+    const contentType = response.headers["content-type"];
     const fileExtension = getFileExtension(contentType);
     const filename = `${path.basename(url)}.${fileExtension}`;
 
-    res.setHeader('Content-Disposition', `attachment; filename=${filename}`);
+    res.setHeader("Content-Disposition", `attachment; filename=${filename}`);
     response.data.pipe(res);
   } catch (error) {
-    console.error('Failed to download screenshot:', error);
-    res.status(500).json({ error: 'Failed to download screenshot' });
+    console.error("Failed to download screenshot:", error);
+    res.status(500).json({ error: "Failed to download screenshot" });
   }
 });
 
 function getFileExtension(contentType) {
   switch (contentType) {
-    case 'image/webp':
-      return 'webp';
-    case 'image/png':
-      return 'png';
-    case 'image/jpeg':
-      return 'jpg';
+    case "image/webp":
+      return "webp";
+    case "image/png":
+      return "png";
+    case "image/jpeg":
+      return "jpg";
     default:
-      return 'bin';
+      return "bin";
   }
 }
 
